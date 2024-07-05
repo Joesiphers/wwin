58a86832793d5cbf7e65cb1adc6d6edffd680fad
Wenguang Zhou <59040826+Joesiphers@users.noreply.github.com>
2024-07-05 00:49:33 +0000

    commit mainpage

--- app/page.tsx
+++ app/page.tsx
@@ -3,21 +3,13 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <div
-        className="relative flex items-center 
-      before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full 
-      before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] 
-      after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic 
-      after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br 
-      before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 
-      after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]"
-      >
+      <div className="mt-24">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          className=""
           src="/plant-drone.jpg"
           alt="WW Coating"
-          width={800}
-          height={533}
+          fill={true}
+          style={{ objectFit: "contain", position: "" }}
           priority
         />
       </div>
@@ -41,26 +33,7 @@ export default function Home() {
           </div>
         </div>
 
-        <div
-          className="relative flex place-items-center 
-      before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full 
-      before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] 
-      after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic 
-      after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br 
-      before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 
-      after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]"
-        >
-          <Image
-            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-            src="/plant-drone.jpg"
-            alt="WW Coating"
-            width={800}
-            height={533}
-            priority
-          />
-        </div>
-
-        <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+        <div className="mb-12 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
           <a
             href="/projects"
             className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
